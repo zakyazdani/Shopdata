0deda16a6c31b778b4332fda0f134d31417a1cbb
zakyazdani <zakariayazdani18@gmail.com>
2024-08-22 18:58:28 +0530

    envadded

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectdb = ()=>{
-    mongoose.connect("mongodb+srv://msfarookhy:KLepAfRNDTllgPsK@shopdata.vb3lx.mongodb.net/")
+    mongoose.connect("process.env.MONGODB_URL")
     .then(() => {
         console.log("Database connection established successfully");
     })
